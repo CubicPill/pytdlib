31dc25778382f0ebe30a69600bf022abb56e2154
CubicPill <huazhengchang97@gmail.com>
2018-02-06 20:43:49 +0800

    remove boost related stuff - will rewrite in cython

--- src/wrappers/td/td_api_wrapper.cpp
+++
@@ -1,8 +0,0 @@
-#include <boost/python.hpp>
-#include "generate/auto/td/telegram/td_api.h"
-
-namespace bp=boost::python;
-
-BOOST_PYTHON_MODULE (tdlib) {
-
-}
--- src/wrappers/tl/TlObject_wrapper.cpp
+++
@@ -1,8 +0,0 @@
-#include <boost/python.hpp>
-#include "tl/TlObject.h"
-
-namespace bp=boost::python;
-
-BOOST_PYTHON_MODULE (tdlib) {
-
-}
